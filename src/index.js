5e1719aa11e99dd41a8effc1dc3a225de8f4052d
root <you@example.com>
2020-01-21 21:49:04 -0300

    add gitkeep file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
@@ -12,12 +13,13 @@ mongoose.connect(`${process.env.MONGO_URL}`, {
   useFindAndModify: false
 });
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 app.use(
   "/files",
   express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
 );
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
 app.use(require("./routes"));
 app.listen(3000);
